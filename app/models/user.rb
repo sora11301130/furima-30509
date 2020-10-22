cd982a3f37bc138eeb99359f3552b3292822cb76
sora11301130 <71582397+sora11301130@users.noreply.github.com>
2020-10-22 17:58:48 +0900

    ユーザー管理機能1

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は、不正な値です。' 
+  validates :reading_first_name, presence: true
+  validates :reading_family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :birthday, presence:true
+
+  has_many :items
+  has_many :comments
+  has_many :orders
+end
