f49762907edf7dadaff8403980abd87fb461f476
sora11301130 <71582397+sora11301130@users.noreply.github.com>
2020-10-23 11:29:28 +0900

    ユーザー管理機能3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,17 @@ class User < ApplicationRecord
   PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は、不正な値です。' 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-   validates :reading_first_name, presence: true
-   validates :reading_family_name, presence: true
    validates :first_name, presence: true
    validates :family_name, presence: true
   end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+   validates :reading_first_name, presence: true
+   validates :reading_family_name, presence: true
+  end
+  
   validates :birthday, presence: true
+
 
   has_many :items
   has_many :comments
