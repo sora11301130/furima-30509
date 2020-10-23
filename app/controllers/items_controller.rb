dbbe8e3015a2185f798c2febe50e01ebb37b5252
sora11301130 <71582397+sora11301130@users.noreply.github.com>
2020-10-23 13:56:50 +0900

    test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,4 @@
-# class ItemsController < ApplicationController
-#   before_action :authenticate_user!, except:[:index, :show]
-
-#   def index
-#     @items = Item.all
-#   end
-# end
+class ItemsController < ApplicationController
+  def index
+  end
+end
