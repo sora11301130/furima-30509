5603f75f20271d0b3c28fb312cdbd1d6401957bf
sora11301130 <71582397+sora11301130@users.noreply.github.com>
2020-10-26 18:45:17 +0900

    商品情報編集機能2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
+  before_action :set_item, only:[:show, :edit, :update]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -19,23 +20,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user.id !=@item.user_id
       redirect_to root_path
-    end
-
-    if current_user.id !=user_signed_in?
-      return new_user_session_path
     end
     
   end
 
   def update
-     @item = Item.find(params[:id])
      if @item.update(item_params)
       redirect_to item_path(@item)
      else
